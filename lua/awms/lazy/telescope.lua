34729c0f9fc400dc19373e30a2728f846c2ea887
Hekser <sikora1595@gmail.com>
2024-09-17 15:45:47 +0200

    Change telescope find files config

--- lua/awms/lazy/telescope.lua
+++ lua/awms/lazy/telescope.lua
@@ -4,6 +4,16 @@ return {
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { ".git/" },
+        },
+        pickers = {
+          find_files = {
+            hidden = true, -- Show hidden files
+          },
+        },
+      })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
