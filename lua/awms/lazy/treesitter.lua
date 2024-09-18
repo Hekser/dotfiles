4661445de8b61827e99d989b3e2150c66f87fce8
Hekser <sikora1595@gmail.com>
2024-09-18 15:57:48 +0200

    Intelli sense

--- lua/awms/lazy/treesitter.lua
+++ lua/awms/lazy/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "javascript", "typescript", "lua", "rust" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "javascript", "typescript", "lua", "rust", "html", "tsx" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
