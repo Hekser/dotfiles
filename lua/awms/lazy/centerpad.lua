62c5db2a92fd4e84ea0d26fcc163137550590b4c
Hekser <sikora1595@gmail.com>
2024-10-01 16:52:59 +0200

    Improve config

--- lua/awms/lazy/centerpad.lua
+++ lua/awms/lazy/centerpad.lua
@@ -1,6 +1,8 @@
 return {
-  "smithbm2316/centerpad.nvim",
-  config = function()
-    vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>Centerpad<cr>", { silent = true, noremap = true })
-  end,
+	{
+		"smithbm2316/centerpad.nvim",
+		config = function()
+			vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>Centerpad<cr>", { silent = true, noremap = true })
+		end,
+	},
 }
--- lua/awms/lazy/colorscheme.lua
+++ lua/awms/lazy/colorscheme.lua
@@ -12,4 +12,21 @@ return {
 	--      vim.o.background = "dark"
 	--    end,
 	--  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+		config = function(_, opts)
+			require("tokyonight").setup(opts)
+			vim.cmd.colorscheme("tokyonight-moon")
+			vim.o.background = "dark"
+		end,
+	},
 }
