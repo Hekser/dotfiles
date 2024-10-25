8ac731600cb63db70ee5a9ebeb4ea51ac0fa0a35
Hekser <sikora1595@gmail.com>
2024-10-25 12:54:00 +0200

    Remove centerpad plugin

--- lua/awms/lazy/centerpad.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{
-		"smithbm2316/centerpad.nvim",
-		config = function()
-			vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>Centerpad<cr>", { silent = true, noremap = true })
-		end,
-	},
-}
