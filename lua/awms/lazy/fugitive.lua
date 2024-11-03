b242b5c8910fbd731b2346bf7d1b6f2f6a908ad9
Hekser <sikora1595@gmail.com>
2024-11-03 21:16:07 +0100

    Telescope git status

--- lua/awms/lazy/fugitive.lua
+++ lua/awms/lazy/fugitive.lua
@@ -1,5 +1,9 @@
 return {
   {
     "tpope/vim-fugitive",
+    config = function()
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "<leader>gs", ":Telescope git_status<CR>", opts)
+    end,
   },
 }
