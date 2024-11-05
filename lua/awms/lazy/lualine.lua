56d3784931899cac7898b1ae1f71e69a4eaae3e9
Hekser <sikora1595@gmail.com>
2024-11-05 13:49:41 +0100

    Lualine - display folder name with full file path

--- lua/awms/lazy/lualine.lua
+++ lua/awms/lazy/lualine.lua
@@ -2,9 +2,24 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     config = function()
+      local lualine_c = {
+        {
+          function()
+            return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+          end,
+        },
+        { "filename", path = 1 }, -- Shows the full path to the file
+      }
+
       require("lualine").setup({
         options = {
           theme = "auto",
+        },
+        sections = {
+          lualine_c = lualine_c,
+        },
+        inactive_sections = {
+          lualine_c = lualine_c,
         },
       })
     end,
