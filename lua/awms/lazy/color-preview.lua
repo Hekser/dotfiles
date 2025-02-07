ad587d5e5cac7d6dc91d2687d0c1d75c5cb51686
Hekser <sikora1595@gmail.com>
2025-02-07 12:41:52 +0100

    Color preview

--- lua/awms/lazy/color-preview.lua
+++ lua/awms/lazy/color-preview.lua
@@ -1,22 +1,32 @@
 return {
-	{
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup({
-				-- Enable colorizer for certain file types (or use "all" for all files)
-				"*", -- Highlight colors in all files by default
-				css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in CSS
-				html = { names = true }, -- Enable named colors in HTML
-			})
-		end,
-	},
-	-- {
-	-- 	"eero-lehtinen/oklch-color-picker.nvim",
-	-- 	event = "VeryLazy",
-	-- 	opts = {},
-	-- 	keys = {
-	-- 		-- One handed keymap recommended, you will be using the mouse
-	-- 		{ "<leader>v", "<cmd>ColorPickOklch<cr>", desc = "Color pick under cursor" },
-	-- 	},
-	-- },
+  -- {
+  --   "uga-rosa/ccc.nvim",
+  --   config = function()
+  --     require("ccc").setup({
+  --       -- your configuration comes here
+  --       -- or leave it empty to use the default settings
+  --       -- refer to the configuration section below
+  --     })
+  --   end,
+  -- },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        -- Enable colorizer for certain file types (or use "all" for all files)
+        "*",                 -- Highlight colors in all files by default
+        css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in CSS
+        html = { names = true }, -- Enable named colors in HTML
+      })
+    end,
+  },
+  -- {
+  -- 	"eero-lehtinen/oklch-color-picker.nvim",
+  -- 	event = "VeryLazy",
+  -- 	opts = {},
+  -- 	keys = {
+  -- 		-- One handed keymap recommended, you will be using the mouse
+  -- 		{ "<leader>v", "<cmd>ColorPickOklch<cr>", desc = "Color pick under cursor" },
+  -- 	},
+  -- },
 }
