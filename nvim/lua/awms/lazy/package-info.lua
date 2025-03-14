b6081c0355809ef6a0f9ef5e1da1436b62fe3474
Hekser <sikora1595@gmail.com>
2025-03-14 22:01:34 +0100

    Package info

--- nvim/lua/awms/lazy/package-info.lua
+++ nvim/lua/awms/lazy/package-info.lua
@@ -10,27 +10,27 @@ return {
       })
 
       -- Show dependency versions
-      vim.keymap.set({ "n" }, "<LEADER>ns", require("package-info").show, { silent = true, noremap = true })
+      vim.keymap.set({ "n" }, "<leader>ns", require("package-info").show, { silent = true, noremap = true })
 
       -- Hide dependency versions
-      vim.keymap.set({ "n" }, "<LEADER>nc", require("package-info").hide, { silent = true, noremap = true })
+      vim.keymap.set({ "n" }, "<leader>nc", require("package-info").hide, { silent = true, noremap = true })
 
       -- Toggle dependency versions
-      vim.keymap.set({ "n" }, "<LEADER>nt", require("package-info").toggle, { silent = true, noremap = true })
+      vim.keymap.set({ "n" }, "<leader>nt", require("package-info").toggle, { silent = true, noremap = true })
 
       -- Update dependency on the line
-      vim.keymap.set({ "n" }, "<LEADER>nu", require("package-info").update, { silent = true, noremap = true })
+      vim.keymap.set({ "n" }, "<leader>nu", require("package-info").update, { silent = true, noremap = true })
 
       -- Delete dependency on the line
-      vim.keymap.set({ "n" }, "<LEADER>nd", require("package-info").delete, { silent = true, noremap = true })
+      vim.keymap.set({ "n" }, "<leader>nd", require("package-info").delete, { silent = true, noremap = true })
 
       -- Install a new dependency
-      vim.keymap.set({ "n" }, "<LEADER>ni", require("package-info").install, { silent = true, noremap = true })
+      vim.keymap.set({ "n" }, "<leader>ni", require("package-info").install, { silent = true, noremap = true })
 
       -- Install a different dependency version
       vim.keymap.set(
         { "n" },
-        "<LEADER>np",
+        "<leader>np",
         require("package-info").change_version,
         { silent = true, noremap = true }
       )
