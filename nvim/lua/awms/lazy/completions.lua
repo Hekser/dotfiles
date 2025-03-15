445d181cace04b42eeefccf00281b46fbea14765
Hekser <sikora1595@gmail.com>
2025-03-15 01:05:32 +0100

    Improve blink

--- nvim/lua/awms/lazy/completions.lua
+++ nvim/lua/awms/lazy/completions.lua
@@ -24,7 +24,11 @@ return {
 			-- C-k: Toggle signature help
 			--
 			-- See the full "keymap" documentation for information on defining your own keymap.
-			keymap = { preset = "default" },
+			-- keymap = { preset = "default" },
+			keymap = {
+				preset = "enter",
+				["<C-y>"] = { "select_and_accept" },
+			},
 
 			appearance = {
 				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -34,6 +38,27 @@ return {
 				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
+			},
+
+			completion = {
+				accept = {
+					-- experimental auto-brackets support
+					auto_brackets = {
+						enabled = true,
+					},
+				},
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+				ghost_text = {
+					enabled = true,
+				},
 			},
 
 			-- Default list of enabled providers defined so that you can extend it
@@ -48,7 +73,7 @@ return {
 			--
 			-- See the fuzzy documentation for more information
 			fuzzy = { implementation = "prefer_rust_with_warning" },
-      signature = { enabled = true },
+			signature = { enabled = true },
 		},
 		opts_extend = { "sources.default" },
 	},
