8cf6dbfea2c01e847578bb9fc3877c10d65d683b
Hekser <sikora1595@gmail.com>
2025-03-19 13:37:58 +0100

    Turn on auto linting

--- nvim/lua/awms/lazy/linting.lua
+++ nvim/lua/awms/lazy/linting.lua
@@ -10,16 +10,17 @@ return {
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
+			astro = { "eslint_d" },
 		}
 
-		-- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-		-- 	group = lint_augroup,
-		-- 	callback = function()
-		-- 		lint.try_lint()
-		-- 	end,
-		-- })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged", "TextChangedI" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
 
 		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
