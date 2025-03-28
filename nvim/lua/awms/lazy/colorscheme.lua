449631b5561fa5c78a2c332ed57e78f54e580500
Hekser <sikora1595@gmail.com>
2025-03-28 11:04:57 +0100

    Switch to rose-pine coloscheme

--- nvim/lua/awms/lazy/colorscheme.lua
+++ nvim/lua/awms/lazy/colorscheme.lua
@@ -1,14 +1,19 @@
 return {
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	name = "rose-pine",
-	-- 	config = function()
-	-- 		require("rose-pine").setup({
-	-- 			dark_variant = "moon",
-	-- 		})
-	-- 		vim.cmd("colorscheme rose-pine")
-	-- 	end,
-	-- },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				dark_variant = "moon",
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = true,
+				},
+			})
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 	--  {
 	--    "catppuccin/nvim",
 	--    name = "catppuccin",
@@ -22,21 +27,21 @@ return {
 	--      vim.o.background = "dark"
 	--    end,
 	--  },
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				--   floats = "transparent",
-			},
-		},
-		config = function(_, opts)
-			require("tokyonight").setup(opts)
-			vim.cmd.colorscheme("tokyonight-moon")
-			vim.o.background = "dark"
-		end,
-	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {
+	-- 		transparent = true,
+	-- 		styles = {
+	-- 			sidebars = "transparent",
+	-- 			--   floats = "transparent",
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("tokyonight").setup(opts)
+	-- 		vim.cmd.colorscheme("tokyonight-moon")
+	-- 		vim.o.background = "dark"
+	-- 	end,
+	-- },
 }
