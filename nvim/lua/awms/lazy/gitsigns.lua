427fa4b25068f71e4fc00c6f039ed26722a31d2a
Hekser <sikora1595@gmail.com>
2025-04-18 08:59:08 +0200

    Improve git blame

--- nvim/lua/awms/lazy/gitsigns.lua
+++ nvim/lua/awms/lazy/gitsigns.lua
@@ -1,11 +1,16 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+				current_line_blame_opts = {
+					delay = 0, -- milliseconds, default is 1000
+				},
+			})
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end,
-  },
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
+	},
 }
