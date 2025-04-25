153eb93aef5a2c7905e374d2380754fef4517354
Hekser <sikora1595@gmail.com>
2025-04-25 13:41:30 +0200

    Improve fzf

--- nvim/lua/awms/lazy/fzf.lua
+++ nvim/lua/awms/lazy/fzf.lua
@@ -1,20 +1,43 @@
 return {
 	{
 		"ibhagwan/fzf-lua",
-		-- optional for icon support
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		-- or if using mini.icons/mini.nvim
 		-- dependencies = { "echasnovski/mini.icons" },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			local fzf = require("fzf-lua")
 
-			fzf.setup({}) -- keep all defaults
+			fzf.setup({
+				defaults = {
+					winopts = {
+						preview = {
+							layout = "vertical", -- horizontal|vertical|flex
+						},
+					},
+					keymap = {
+						builtin = {
+							["<C-d>"] = "preview-page-down",
+							["<C-u>"] = "preview-page-up",
+						},
+					},
+				},
 
-			-- override just specific keybindings
-      fzf.defaults.winopts.preview.layout = "vertical" -- horizontal|vertical|flex
-			fzf.defaults.keymap.builtin["<C-d>"] = "preview-page-down"
-			fzf.defaults.keymap.builtin["<C-u>"] = "preview-page-up"
+				files = {
+					formatter = "path.filename_first",
+				},
 
+				grep = {
+					rg_opts = "--sort path --column --line-number --no-heading --color=always -S",
+					fzf_opts = {
+						["--no-sort"] = "",
+						["--tiebreak"] = "index",
+						["--delimiter"] = ":",
+						["--nth"] = "2..",
+						["--layout"] = "reverse",
+					},
+				},
+			})
+
+			-- keymaps
 			vim.keymap.set("n", "<leader>ff", fzf.files, {})
 			vim.keymap.set("n", "<leader>fg", fzf.live_grep, {})
 			vim.keymap.set("n", "<leader>fm", fzf.marks, {})
