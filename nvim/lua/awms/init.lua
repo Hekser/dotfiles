71b8ac3dc481ee46e98bf6aac49012ba54fed3f5
Hekser <sikora1595@gmail.com>
2025-10-01 22:56:49 +0200

    Change neovim line number color

--- nvim/lua/awms/init.lua
+++ nvim/lua/awms/init.lua
@@ -3,5 +3,5 @@ require("awms.keymap")
 require("awms.lazy_init")
 
 vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#00b7ff" }) -- Replace with desired color
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#446688" }) -- Above cursor
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#446688" }) -- Below cursor
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#6e7681" }) -- Above cursor
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#6e7681" }) -- Below cursor
